de466adf1e0eb4c934d36c31f81490a75caa6e5b
AmarthiaDinarKusminda <amarthiadinar99@gmail.com>
2020-11-14 13:58:44 +0700

    menambah file profile

--- project_mobile_app/lib/profile.dart
+++ project_mobile_app/lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_mobile_app/home.dart';
+import 'package:project_mobile_app/dashboard.dart';
+import 'dashboard.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
@@ -9,15 +10,88 @@ class ProfilePage extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.deepOrange,
-          title: Text("INI HALAMAN PROFIL"),
+          title: Text("PROFIL"),
         ),
-        body: Center(
-          child: RaisedButton(child: Text("BACK TO DASHBOARD"),
-          onPressed: (){
-            Navigator.pop(context, MaterialPageRoute(builder: (context){
-              return MainPage();
-            }));
-          },)
+        body: Column(
+          children:<Widget> [
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.only(left:5.0),
+              height: 50,
+              decoration: BoxDecoration(color: Colors.deepOrange, borderRadius: BorderRadius.circular(5.0)),
+              child:Row(
+                children:<Widget> [
+                  Image.asset("images/assets_akun.png", width: 20.0, height: 20.0,),
+                  Text(" Akun",textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.only(left:5.0),
+              height: 50,
+              decoration: BoxDecoration(color: Colors.deepOrange, borderRadius: BorderRadius.circular(5.0)),
+              child: Row(
+                children:<Widget> [
+                  Image.asset("images/assets_bahasa.png", width: 20.0, height: 20.0,),
+                  Text(" Bahasa",
+                      style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white)),
+                ],
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.only(left:5.0),
+              height: 50,
+              decoration: BoxDecoration(color: Colors.deepOrange, borderRadius: BorderRadius.circular(5.0)),
+              child: Row(
+                children: <Widget>[
+                  Image.asset("images/assets_bantuan.png", width: 20.0, height: 20.0,),
+                  Text(" Bantuan",
+                      style: TextStyle(
+                          fontSize:22,
+                          color: Colors.white)),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.only(left:5.0),
+              height: 50,
+              decoration: BoxDecoration(color: Colors.deepOrange, borderRadius: BorderRadius.circular(5.0)),
+              child:Row(
+                children:<Widget> [
+                  Image.asset("images/assets_logout.png",width: 20.0, height: 20.0),
+                  Text(" Keluar",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,)),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top:300.0),
+              alignment:Alignment.bottomRight,
+              child: RaisedButton(child: Text("BACK TO DASHBOARD"),
+                onPressed: (){
+                  Navigator.pop(context, MaterialPageRoute(builder: (context){
+                    return Dashboard();
+                  }));
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
