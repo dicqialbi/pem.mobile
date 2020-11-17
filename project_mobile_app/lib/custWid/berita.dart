c8b129e1f866123d7d4995c70b0ccd6675a58ed5
AmarthiaDinarKusminda <amarthiadinar99@gmail.com>
2020-11-18 02:07:23 +0700

    menambah file berita

--- project_mobile_app/lib/custWid/berita.dart
+++ project_mobile_app/lib/custWid/berita.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project_mobile_app/custWid/news.dart';
+import 'package:project_mobile_app/custWid/berita1.dart';
+import 'package:project_mobile_app/custWid/berita2.dart';
+import 'package:project_mobile_app/custWid/berita3.dart';
 
 class Berita extends StatefulWidget {
   Berita();
@@ -22,7 +25,6 @@ class _BeritaState extends State<Berita> {
                   fontWeight: FontWeight.bold,
                   fontSize: 15,
                   fontStyle: FontStyle.italic),
-              textAlign: TextAlign.end,
             )),
         Container(
           decoration: BoxDecoration(
@@ -37,22 +39,40 @@ class _BeritaState extends State<Berita> {
             ],
           ),
           child: Column(
-            children: <Widget>[
-              NewsCard(
-                  "Pemerintah Desa Suka Maju Bagi Bagi Sembako",
-                  "Presiden Joko Widodo (Jokowi) mengatakan pembagian paket sembako dan Kartu Prakerja akan dilakukan oleh Menteri Sosial Juliari P. Batubara. Pembagian ini sudah mengikuti ketentuan tambah anggaran bagi masing-masing program.",
-                  AssetImage("images/news.jpeg")),
-              NewsCard(
-                  "Demo di Kedubes Prancis Bubar, Jalan MH Thamrin ke Bundaran HI Tersendat",
-                  "Pantauan detikcom, Jumat (6/11/2020), satu mobil komando dengan spanduk Pemuda Organization of Islamic Cooperation (OIC Youth) Indonesia datang dari arah Patung Kuda Arjuna Wiwaha ke Jalan MH Thamrin, tepatnya di dekat halte TransJakarta Sarinah sekitar pukul 13.19 WIB. Mobil komando ini datang tanpa diikuti massa aksi.",
-                  AssetImage("images/demo.jpg")),
-              NewsCard(
-                  "Telkomsel Bagi-bagi Paket Internet Murah Sampai Kuota Gratis, 10 GB Rp 2 Ribuan",
-                  "Segera daftarkan bro kode promo Paket Internet Murah 30GB Cuma Rp 5 Ribuan, kini ada Promo Telkomsel 10GB Cuma Rp 2.700 dan 25GB Cuma Rp 10 Ribu. Beragam Promo Telkomsel dan Paket Internet Murah dan Kuota Gratis dari Telkomsel.",
-                  AssetImage("images/telk.png"))
-            ],
-          ),
-        )
+                  children: <Widget>[
+                    InkWell(onTap: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => berita1()),
+                      );
+                    },
+                     child: NewsCard(
+                      "Pemerintah Desa Suka Maju Bagi Bagi Sembako",
+                      "Presiden Joko Widodo (Jokowi) mengatakan pembagian paket sembako dan Kartu Prakerja akan dilakukan oleh Menteri Sosial Juliari P. Batubara. Pembagian ini sudah mengikuti ketentuan tambah anggaran bagi masing-masing program.",
+                      AssetImage("images/news.jpeg"))
+                    ),
+                    InkWell(onTap: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => berita2()),
+                      );
+                    },
+                        child: NewsCard(
+                            "Demo di Kedubes Prancis Bubar, Jalan MH Thamrin ke Bundaran HI Tersendat",
+                            "Pantauan detikcom, Jumat (6/11/2020), satu mobil komando dengan spanduk Pemuda Organization of Islamic Cooperation (OIC Youth) Indonesia datang dari arah Patung Kuda Arjuna Wiwaha ke Jalan MH Thamrin, tepatnya di dekat halte TransJakarta Sarinah sekitar pukul 13.19 WIB. Mobil komando ini datang tanpa diikuti massa aksi.",
+                            AssetImage("images/demo.jpg"))
+                    ),
+                    InkWell(onTap: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => berita3()),
+                      );
+                    },
+                        child: NewsCard(
+                            "Telkomsel Bagi-bagi Paket Internet Murah Sampai Kuota Gratis, 10 GB Rp 2 Ribuan",
+                            "Segera daftarkan bro kode promo Paket Internet Murah 30GB Cuma Rp 5 Ribuan, kini ada Promo Telkomsel 10GB Cuma Rp 2.700 dan 25GB Cuma Rp 10 Ribu. Beragam Promo Telkomsel dan Paket Internet Murah dan Kuota Gratis dari Telkomsel.",
+                            AssetImage("images/telk.png"))
+                    ),
+                  ],
+        ),
+        ),
       ],
     );
   }
