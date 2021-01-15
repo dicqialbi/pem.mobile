5e9b707c73e4dc06cd14b1be9bd76b1fec259c9f
AmarthiaDinarKusminda <amarthiadinar99@gmail.com>
2021-01-15 16:07:44 +0700

    mengubah tampilan

--- project_mobile_app/lib/form.dart
+++ project_mobile_app/lib/form.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() => runApp(MyForm());
 
@@ -6,17 +8,40 @@ class MyForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appTitle = 'Form Surat';
-
     return MaterialApp(
       title: appTitle,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.deepOrange,
-          title: Text(appTitle),
-        ),
+          title:Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    splashColor: Colors.deepOrange,
+                    onTap: () {
+                      Navigator.pop(context,
+                          MaterialPageRoute(builder: (context) {
+                            return MainPage();
+                          }));
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ),
+                  Text("Form Surat",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20))
+                ]),
+         ),
         body: MyCustomForm(),
-      ),
-    );
+      ));
   }
 }
 
@@ -28,105 +53,170 @@ class MyCustomForm extends StatefulWidget {
   }
 }
 
-// Create a corresponding State class.
-// This class holds data related to the form.
 class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-             decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Jenis Surat?'
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(2),
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black54.withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 2,
+                      offset: Offset(1, 1))
+                ],
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Jenis Surat'
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Harap diisi';
+                  }
+                  return null;
+                },
+              ),
             ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Harap diisi';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-             decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Nama Lengkap'
+            Container(
+              margin: EdgeInsets.all(2),
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black54.withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 2,
+                      offset: Offset(1, 1))
+                ],
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Nama Lengkap'
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Harap diisi';
+                  }
+                  return null;
+                },
+              ),
             ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Harap diisi';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-             decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Jenis Kelamin'
+            Container(
+              margin: EdgeInsets.all(2),
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black54.withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 2,
+                      offset: Offset(1, 1))
+                ],
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Jenis Kelamin'
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Harap diisi';
+                  }
+                  return null;
+                },
+              ),
             ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Harap diisi';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-             decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Agama'
+            Container(
+              margin: EdgeInsets.all(2),
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black54.withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 2,
+                      offset: Offset(1, 1))
+                ],
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Agama'
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Harap diisi';
+                  }
+                  return null;
+                },
+              ),
             ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Harap diisi';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-             decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Alamat'
+            Container(
+              margin: EdgeInsets.all(2),
+              padding: EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black54.withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 2,
+                      offset: Offset(1, 1))
+                ],
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Alamat'
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Harap diisi';
+                  }
+                  return null;
+                },
+              ),
             ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Harap diisi';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false
-                // otherwise.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a Snackbar.
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Processing Data')));
-                }
-              },
-              child: Text('Submit'),
-              style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrangeAccent),
-  ),
-              
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false
+                  // otherwise.
+                  if (_formKey.currentState.validate()) {
+                    // If the form is valid, display a Snackbar.
+                    Scaffold.of(context)
+                        .showSnackBar(SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: Text('Submit'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
+
